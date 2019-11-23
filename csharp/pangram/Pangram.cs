a9f5ae1c5b7ce0812a125793795242e61b3b74e8
Antoine Macia <antoine@discolabs.com>
2019-11-23 22:32:53 +1100

    C# Pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        IDictionary<char, int> charactersDict = new Dictionary<char, int>();
+        foreach (char character in input.ToLower())
+        {
+            if(Char.IsLetter(character) && !charactersDict.ContainsKey(character))
+            {
+                charactersDict.Add(new KeyValuePair<char, int>(character, 1));
+            }
+        }
+        return charactersDict.Count >= 26;
+    }
+}
